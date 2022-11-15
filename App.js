a4ced8573e8d113517f2b8e13c9e15b84e393431
Cleviane Rebeca <rebecacleviane@gmail.com>
2022-11-15 15:02:11 +0100

    small fixes

--- App.js
+++ App.js
@@ -5,12 +5,3 @@ import Categories from "./screens/Categories";
 export default function App() {
   return <Categories />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- data/dummy.js
+++ data/dummy.js
@@ -2,10 +2,10 @@ import Category from "../models/category";
 import Meal from "../models/meal";
 
 export const CATEGORIES = [
-  new Category("c1", "Italian", "#f5428d"),
-  new Category("c2", "Quick & Easy", "#f54242"),
-  new Category("c3", "Hamburgers", "#f5a442"),
-  new Category("c4", "German", "#f5d142"),
+  new Category("c1", "Italian", "#f35d9c"),
+  new Category("c2", "Quick & Easy", "#f56f46"),
+  new Category("c3", "Hamburgers", "#f6be30"),
+  new Category("c4", "German", "#dbf935"),
   new Category("c5", "Light & Lovely", "#368dff"),
   new Category("c6", "Exotic", "#41d95d"),
   new Category("c7", "Breakfast", "#9eecff"),
