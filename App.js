d56d1ae1fb46c19966ed462ff97253b4ccc1b78a
Cleviane Rebeca <rebecacleviane@gmail.com>
2022-11-21 10:52:51 +0100

    adding favorites screen and drawer

--- App.js
+++ App.js
@@ -1,27 +1,61 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
 
 import Categories from "./screens/Categories";
 import Meals from "./screens/Meals";
 import MealDetails from "./screens/MealDetails";
+import Favorites from "./screens/Favorites";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const DrawerNavigator = () => (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#f89393" },
+        headerTintColor: "white",
+      }}
+    >
+      <Drawer.Screen
+        name="DCategories"
+        component={Categories}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <AntDesign name="bars" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <AntDesign name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: "#f89393" },
           headerTintColor: "white",
-          contentStyle: { backgroundColor: "#fffafa" },
         }}
       >
         <Stack.Screen
           name="Categories"
-          component={Categories}
+          component={DrawerNavigator}
           options={{
-            title: "All Categories",
+            headerShown: false,
           }}
         />
         <Stack.Screen
---
+++ screens/Favorites/index.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Favorites = ({ navigation }) => {
+  return <Text>Oi</Text>;
+};
+
+export default Favorites;
